9495d770c4adfbd16541fa390d6677e2848640ff
f2mars <norkuse@gmail.com>
2022-07-18 19:07:53 +0300

    fix trunc for ts and start creating chansesData

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,32 @@ let gameFiledAsHtml = "";
 makeGameField();
 document.body.append(gameFiled);
 
+type NumToNumObj = {
+  [key: number]: number;
+};
+
+class chansesData {
+  private chansesByRow: NumToNumObj = {};
+  private chansesByColumn: NumToNumObj = {};
+  private chansesByBlock: NumToNumObj = {};
+  private fillChansesStructure() {
+    for (let i = 0; i < 9; i++) {
+      [this.chansesByRow, this.chansesByColumn, this.chansesByBlock].forEach(
+        (item) => {
+          item[i] = 9;
+        }
+      );
+    }
+  }
+  constructor() {
+    this.fillChansesStructure();
+  }
+}
+
+let testData = new chansesData();
+
+// --------------- FUNCTIONS -----------------
+
 function makeGameField() {
   for (let row = 0; row < 9; row++) {
     for (let column = 0; column < 9; column++) {
@@ -19,8 +45,8 @@ function makeGameField() {
 }
 
 function identifyBlock(row: number, column: number) {
-  const blocki: number = Math.trunc(row / 3);
-  const blockj: number = Math.trunc(column / 3);
+  const blocki: number = Math.floor(row / 3);
+  const blockj: number = Math.floor(column / 3);
   return 3 * blocki + blockj;
 }
 
